2fd467e71f26986f9da0c0057eb4d7b8498767f2
Qingping Hou <qihou@linkedin.com>
2017-05-15 15:48:33 -0700

    ci: fix mysql and iris setup

--- .ci/run_mysql_docker.sh
+++ .ci/run_mysql_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "[*] Spinning up mysql through docker"
-docker run -p 3306:3306 --name mysql \
-	-e MYSQL_ALLOW_EMPTY_PASSWORD=1  \
-	-e MYSQL_ROOT_HOST=172.17.0.1 \
+docker run -d --name mysql \
+	-e MYSQL_ROOT_PASSWORD=admin  \
+	-e MYSQL_ROOT_HOST=% \
 	-d mysql/mysql-server:5.7
