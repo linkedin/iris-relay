d844b7da8eab226dcdb27d4ab793f741c9a670e1
Qingping Hou <qihou@linkedin.com>
2017-05-15 15:48:33 -0700

    ci: fix mysql and iris setup

--- .ci/install.sh
+++ .ci/install.sh
@@ -35,30 +35,12 @@ pushd ${TRAVIS_BUILD_DIR}
 	travis_retry pip install -r dev_requirements.txt
 popd
 
-
-echo "[*] setup mysql with schema and dummy data..."
-IRIS_REPO_URL=https://raw.githubusercontent.com/linkedin/iris/master
-
-travis_retry wget "${IRIS_REPO_URL}/.ci/setup_mysql.sh"
-mkdir db
-pushd db
-	travis_retry wget "${IRIS_REPO_URL}/db/schema_0.sql"
-	travis_retry wget "${IRIS_REPO_URL}/db/dummy_data.sql"
-popd
-ls -l db
-bash ./setup_mysql.sh
-
-echo "[*] run iris instance for e2e test..."
-mkdir -p iris/config
-pushd iris
-	wget "${IRIS_REPO_URL}/configs/config.dev.yaml" > config/config.yaml
-popd
-
-mkdir -p iris/logs/{nginx,uwsgi}
-
-docker run -p 16649:16649 \
-	-v `pwd`/iris/config:/home/iris/config \
-	quay.io/iris/iris
+echo "[*] spin up iris api..."
+docker run -d -p 16649:16649 \
+    --link mysql \
+    -e DOCKER_DB_BOOTSTRAP=1 \
+    -e IRIS_CFG_DB_USER=root -e IRIS_CFG_DB_PASSWORD=admin -e IRIS_CFG_DB_HOST=mysql \
+    quay.io/iris/iris
 
 echo "[*] create healthcheck status file for relay"
 echo 'GOOD' > /tmp/relay_health
