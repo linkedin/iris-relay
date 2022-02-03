0a89104191d8ffad4ebe52998717e694767cebfd
Diego Cepeda <dcepeda@linkedin.com>
2022-02-03 11:05:17 -0800

    Update __init__.py

--- src/iris_relay/__init__.py
+++ src/iris_relay/__init__.py
@@ -1,4 +1,4 @@
 # Copyright (c) LinkedIn Corporation. All rights reserved. Licensed under the BSD-2 Clause license.
 # See LICENSE in the project root for license information.
 
-__version__ = '1.2.5'
+__version__ = '1.2.6'
